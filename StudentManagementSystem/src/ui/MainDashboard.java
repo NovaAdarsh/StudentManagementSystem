9c33cfee6a8bdb7538a22ee9e39cf98fd62564d3
NovaAdarsh <satyamkumaroradarsh@gmail.com>
2026-01-04 12:59:48 +0530

    Update MainDashboard.java

--- StudentManagementSystem/src/ui/MainDashboard.java
+++ StudentManagementSystem/src/ui/MainDashboard.java
@@ -1,23 +1,34 @@
 package ui;
+
 import javax.swing.*;
-import java.awt.event.*;
+
 public class MainDashboard extends JFrame {
-    public MainDashboard(){
+
+    public MainDashboard() {
         setTitle("Student Management - Dashboard");
-        setSize(500,300);
+        setSize(500, 300);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(null);
+
         JLabel heading = new JLabel("Student Management System");
-        heading.setBounds(140,10,250,30);
+        heading.setBounds(140, 10, 250, 30);
         add(heading);
+
         JButton addBtn = new JButton("Add Student");
-        addBtn.setBounds(50,80,150,40);
+        addBtn.setBounds(50, 80, 150, 40);
         add(addBtn);
+
         JButton viewBtn = new JButton("View Students");
-        viewBtn.setBounds(300,80,150,40);
+        viewBtn.setBounds(300, 80, 150, 40);
         add(viewBtn);
+
+        JButton updateBtn = new JButton("Update Student");
+        updateBtn.setBounds(170, 150, 160, 40);
+        add(updateBtn);
+
         addBtn.addActionListener(e -> new AddStudentFrame().setVisible(true));
         viewBtn.addActionListener(e -> new ViewStudentFrame().setVisible(true));
+        updateBtn.addActionListener(e -> new UpdateStudentFrame().setVisible(true));
     }
 }
